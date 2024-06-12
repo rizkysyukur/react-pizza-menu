981f4920b9d1ee72399a667bb5530c5feb77cfbb
rizkysyukur <rizkysyukur10@gmail.com>
2024-06-12 20:44:36 +0700

    completing pizza menu

--- src/index.js
+++ src/index.js
@@ -9,7 +9,43 @@ const pizzaData = [
         price: 6,
         photoName: "pizzas/focaccia.jpg",
         soldOut: false
-    }
+    },
+    {
+        name: "Pizza Margherita",
+        ingredients: "Tomato and mozarella",
+        price: 10,
+        photoName: "pizzas/margherita.jpg",
+        soldOut: false
+    },
+    {
+        name: "Pizza Spinaci",
+        ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+        price: 12,
+        photoName: "pizzas/spinaci.jpg",
+        soldOut: false
+    },
+    {
+        name: "Pizza Funghi",
+        ingredients: "Tomato, mozarella, mushrooms, and onion",
+        price: 12,
+        photoName: "pizzas/funghi.jpg",
+        soldOut: false
+    },
+    {
+        name: "Pizza Salamino",
+        ingredients: "Tomato, mozarella, and pepperoni",
+        price: 15,
+        photoName: "pizzas/salamino.jpg",
+        soldOut: true
+    },
+    {
+        name: "Pizza Prosciutto",
+        ingredients: "Tomato, mozarella, ham, aragula, and burrate cheese",
+        price: 18,
+        photoName: "pizzas/prosciutto.jpg",
+        soldOut: false
+    },
+
 ]
 
 const App = () => (
@@ -31,7 +67,45 @@ const Header = () => {
 }
 
 const Menu = () => {
-    return (<h1>Menu</h1>)
+    const pizzas = pizzaData;
+
+    const numPizzas = pizzas.length;
+
+    return (
+        <main className="menu">
+            <h2>Our menu</h2>
+
+            {numPizzas > 0 ? (
+                <>
+                    <p>
+                        Authentic Italian cuisine. 6 creative dishes to choose from. All
+                        from our stone oven, all organic, all delicious.
+                    </p>
+
+                    <ul className="pizzas">
+                        {pizzas.map((pizza) => (
+                            <Pizza pizzaObj={pizza} key={pizza.name} />
+                        ))}
+                    </ul>
+                </>
+            ) : (
+                <p>We're still working on our menu. Please come back later:</p>
+            )}
+        </main>
+    )
+}
+
+function Pizza({ pizzaObj }) {
+    return (
+        <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+            <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+            <div>
+                <h3>{pizzaObj.name}</h3>
+                <p>{pizzaObj.ingredients}</p>
+                <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
+            </div>
+        </li>
+    );
 }
 
 const Footer = () => {
@@ -52,23 +126,23 @@ const Footer = () => {
                 )
             }
         </footer>
-    )
+    );
 }
 
 const Order = ({ closeHour, openHour }) => {
     return (
         <div className="order">
             <p>
-                we're open from {openHour}:00 to {closeHour}:00. Come visit us or order online.
+                We're open from {openHour}:00 to {closeHour}:00. Come visit us or order online.
             </p>
             <button className="btn">Order</button>
         </div>
-    )
+    );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
-)
+);
